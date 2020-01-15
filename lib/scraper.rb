6a1786c7994a8e1dc63afdf1075d5a4277fbe74a
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 11:11:05 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,12 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+Nokogiri::HTML(html)
+require 'nokogiri'
+require 'open-uri'
+ 
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-26OIBN")
+
+doc.css(".headline-26OIBN").text
